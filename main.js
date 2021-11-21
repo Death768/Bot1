1c28eba837cf9fdc56c6e6a8e84654aa873f14eb
Death768 <yiyoung.md@gmail.com>
2021-11-20 20:03:35 -0600

    event handler

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./token.json');
+const fs = require('fs');
 
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-bot.once('ready', () => {
-	console.log(`Ready!`);
-});
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for(const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if(event.once) {
+		bot.once(event.name, (...args) => event.execute(...args));
+	} else {
+		bot.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 bot.login(token);
