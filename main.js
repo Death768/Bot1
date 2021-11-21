0f25e836465054bee92304b7c975b48f49109b4b
Death768 <yiyoung.md@gmail.com>
2021-11-20 20:16:24 -0600

    command handler

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./token.json');
 const fs = require('fs');
 
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-
 for(const file of eventFiles) {
 	const event = require(`./events/${file}`);
 	if(event.once) {
@@ -13,6 +12,13 @@ for(const file of eventFiles) {
 	} else {
 		bot.on(event.name, (...args) => event.execute(...args));
 	}
+}
+
+bot.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
 }
 
 bot.login(token);
