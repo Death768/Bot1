646c839fe361aac9969e889b868d0d5638548891
Yiyoung <yiyoung.md@gmail.com>
2022-01-03 10:17:52 -0600

    added a target option for balance command

--- commands/balance.js
+++ commands/balance.js
@@ -5,21 +5,30 @@ const Wallet = require('../models/wallet.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('balance')
-		.setDescription('shows your balance'),
+		.setDescription('shows your balance')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('check user\'s balance')),
 	async execute(interaction) {
+		let target = interaction.options._hoistedOptions[0] || interaction;
+
+		if(target.user.bot) {
+			interaction.reply(`You cannot check the balance of a bot.`);
+			return;
+		}
 		Wallet.findOne({
-			user_id: interaction.user.id,
+			user_id: target.user.id,
 			guild_id: interaction.guild.id
 		}, (err, wallet) => {
 			if(err) console.log(err);
 
 			let embed = new MessageEmbed()
-				.setColor(interaction.member.displayHexColor)
+				.setColor(target.member.displayHexColor)
 				.setTitle("Balance")
-				.setThumbnail(interaction.user.displayAvatarURL());
+				.setThumbnail(target.user.displayAvatarURL());
 
 			if(!wallet) {
-				embed.addField("Coins", 0, true);
+				embed.addField("Coins", '0', true);
 			} else {
 				embed.addField("Coins", wallet.balance.toString(), true);
 			}
